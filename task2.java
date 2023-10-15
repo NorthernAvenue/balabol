08658baa9e182ee18f5d84869792d5b29aace0b8
NorthernAvenue <NorthernAvenueAI@gmail.com>
2023-10-15 16:24:56 +0300

    2

---
+++ task2.java
@@ -0,0 +1,10 @@
+// public class task2 {
+//     try {
+//    int d = 0;
+//    double catchedRes1 = 1.0 / d; // Деление на 0 должно быть с плавающей точкой
+//    System.out.println("catchedRes1 = " + catchedRes1);
+// } catch (ArithmeticException e) {
+//    System.out.println("Catching exception: " + e);
+// }
+// }
+
